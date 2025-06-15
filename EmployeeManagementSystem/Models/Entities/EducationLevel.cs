699a52ab2aa6ac782ee8dfb16204eb306ccd292b
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-15 12:39:07 +0330

    EducationLevel entity created.

---
+++ EmployeeManagementSystem/Models/Entities/EducationLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagementSystem.Models.Entities
+{
+    public class EducationLevel : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
