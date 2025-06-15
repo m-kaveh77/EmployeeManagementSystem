e58df80eea65dd0b6fda76228418085a8702b6c2
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-15 23:24:14 +0330

    EmployeeService for database operations created.

---
+++ EmployeeManagementSystem/Services/EmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Services
+{
+    public class EmployeeService(EmployeeDbContext context)
+    {
+        public async Task<int> GetAllEmployeeCountAsync()
+        {
+            return await context.Employees.CountAsync();
+        }
+
+        public async Task<int> GetActiveEmployeeCountAsync()
+        {
+            return await context.Employees.CountAsync(e => e.IsActive);
+        }
+    }
+}
