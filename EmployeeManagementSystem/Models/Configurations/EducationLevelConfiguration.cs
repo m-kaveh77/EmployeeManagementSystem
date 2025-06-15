428d4ddd84492de64dbde78bf35bf73191de58d1
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-15 12:38:06 +0330

    EducationLevelConfiguration created.

---
+++ EmployeeManagementSystem/Models/Configurations/EducationLevelConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagementSystem.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeManagementSystem.Models.Configurations
+{
+    public class EducationLevelConfiguration : IEntityTypeConfiguration<EducationLevel>
+    {
+        public void Configure(EntityTypeBuilder<EducationLevel> builder)
+        {
+            builder.Property(e => e.Title).HasMaxLength(100);
+        }
+    }
+}
