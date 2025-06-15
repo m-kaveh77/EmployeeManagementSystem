eadf4c7a0ec346d2091baf8bc29879e465b67629
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-15 12:41:28 +0330

    OnStartup and AddServices methods added.

--- EmployeeManagementSystem/App.xaml.cs
+++ EmployeeManagementSystem/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using EmployeeManagementSystem.Infrastructure;
+using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Services;
+using EmployeeManagementSystem.Windows.Login;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace EmployeeManagementSystem
@@ -9,6 +14,29 @@ namespace EmployeeManagementSystem
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var appBuilder = Host.CreateApplicationBuilder(e.Args);
+            appBuilder.Configuration.AddJsonFile("appsettings.json");
+            appBuilder.Services.AddHostedService<AppHostedService>();
 
+            AddServices(appBuilder.Services);
+
+            var host = appBuilder.Build();
+            await host.RunAsync();
+        }
+
+        private void AddServices(IServiceCollection services)
+        {
+            services.AddDbContext<EmployeeDbContext>(ServiceLifetime.Transient);
+            
+            services.AddTransient<UserService>();
+
+            // ViewModels
+
+
+            // Windows
+            services.AddTransient<LoginWindow>();
+        }
+    }
 }
