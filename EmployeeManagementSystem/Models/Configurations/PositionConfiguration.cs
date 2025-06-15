f93505524129b0eeec19479159675542c385c315
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-15 12:38:25 +0330

    PositionConfiguration created.

---
+++ EmployeeManagementSystem/Models/Configurations/PositionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementSystem.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeManagementSystem.Models.Configurations
+{
+    public class PositionConfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.HasOne(p => p.ParentPosition)
+                .WithMany(p => p.Positions)
+                .HasForeignKey(p => p.ParentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(p => p.Title).HasMaxLength(200);
+        }
+    }
+}
