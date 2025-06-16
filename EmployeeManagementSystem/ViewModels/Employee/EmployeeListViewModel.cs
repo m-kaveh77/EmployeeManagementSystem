67d0a236faa12a12f5cd2815b2a9cab729d2b96f
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-16 13:16:44 +0330

    EmployeeListViewModel created.

---
+++ EmployeeManagementSystem/ViewModels/Employee/EmployeeListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagementSystem.Models;
+using System.Collections.ObjectModel;
+
+namespace EmployeeManagementSystem.ViewModels.Employee
+{
+    public class EmployeeListViewModel : BaseViewModel
+    {
+        private readonly EmployeeDbContext _context;
+        public ObservableCollection<Models.Entities.Employee> Employees { get; set; } = new();
+
+        public EmployeeListViewModel(EmployeeDbContext context)
+        {
+            _context = context;
+            Employees = new ObservableCollection<Models.Entities.Employee>(context.Employees.ToList());
+
+            ReloadItems();
+        }
+
+        public void ReloadItems()
+        {
+            Employees = new ObservableCollection<Models.Entities.Employee>(_context.Employees
+                .Select(b => new Models.Entities.Employee
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Family = b.Family,
+                    Mobile = b.Mobile,
+                    NationalCode = b.NationalCode,
+                    Avatar = b.Avatar,
+                    Gender= b.Gender,
+                    //Author = b.Authors.FirstOrDefault().Name
+                }).ToList());
+        }
+    }
+}
