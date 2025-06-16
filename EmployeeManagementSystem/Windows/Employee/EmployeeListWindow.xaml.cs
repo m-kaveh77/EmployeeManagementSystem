9dfbd2ace663849cc60cef2323ebd6299a9d3daa
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-16 13:17:11 +0330

    EmployeeListWindow created.

---
+++ EmployeeManagementSystem/Windows/Employee/EmployeeListWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagementSystem.ViewModels.Employee;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace EmployeeManagementSystem.Windows.Employee
+{
+    /// <summary>
+    /// Interaction logic for EmployeeListWindow.xaml
+    /// </summary>
+    public partial class EmployeeListWindow : Window
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly EmployeeListViewModel _listViewModel;
+
+        public EmployeeListWindow(EmployeeListViewModel listViewModel, IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _listViewModel = listViewModel;
+            DataContext = listViewModel;
+            InitializeComponent();
+        }
+
+        private void AddNewEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            var editWindow = _serviceProvider.GetService<EmployeeEditWindow>();
+            var showDialog = editWindow?.ShowDialog();
+
+            if (showDialog ?? false)
+            {
+                _listViewModel.ReloadItems();
+                EmployeeDataGrid.ItemsSource = null;
+                EmployeeDataGrid.ItemsSource = _listViewModel.Employees;
+            }
+        }
+    }
+}
