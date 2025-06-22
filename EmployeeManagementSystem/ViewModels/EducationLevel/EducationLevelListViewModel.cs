b080959a39170b0af203e281075fe89fdadcc9c2
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-22 11:52:44 +0330

    EducationLevelListViewModel created.

---
+++ EmployeeManagementSystem/ViewModels/EducationLevel/EducationLevelListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+
+namespace EmployeeManagementSystem.ViewModels.EducationLevel
+{
+    public class EducationLevelListViewModel : BaseViewModel
+    {
+        public ObservableCollection<Models.Entities.EducationLevel> EducationLevels { get; set; }
+        private readonly EmployeeDbContext _context;
+        public event Action<Models.Entities.Employee> OnSubmitChangesSuccess;
+
+        public EducationLevelListViewModel(EmployeeDbContext context)
+        {
+            _context = context;
+            EducationLevels = new ObservableCollection<Models.Entities.EducationLevel>(context.EducationLevels.ToList());
+
+            ReloadItems();
+        }
+
+        public void ReloadItems()
+        {
+            EducationLevels = new ObservableCollection<Models.Entities.EducationLevel>(_context.EducationLevels
+                .AsNoTracking()
+                .ToList());
+        }
+    }
+}
